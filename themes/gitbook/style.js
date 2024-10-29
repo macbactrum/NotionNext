6bb5d60b6f71ab719791c6e75e80d11e6a4ae353
macbactrum <150027680+macbactrum@users.noreply.github.com>
2024-10-29 13:59:05 +0700

    Update style.js
    translate

--- themes/gitbook/style.js
+++ themes/gitbook/style.js
@@ -1,13 +1,13 @@
 /* eslint-disable react/no-unknown-property */
 /**
- * 此处样式只对当前主题生效
- * 此处不支持tailwindCSS的 @apply 语法
+ * The style here only takes effect on the current theme
+ * The @apply syntax of tailwindCSS is not supported here.
  * @returns
  */
 const Style = () => {
   return <style jsx global>{`
     
-    // 底色
+    // background color
     .dark body{
         background-color: black;
     }
